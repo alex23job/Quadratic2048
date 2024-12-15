014caf7ce861ff0049b9bef45d218f47a651efcf
alex23job <106354657+alex23job@users.noreply.github.com>
2024-12-16 02:24:33 +0300

    Adding button down

--- Quadrat2048/Assets/Scripts/LevelControl.cs
+++ Quadrat2048/Assets/Scripts/LevelControl.cs
@@ -9,7 +9,7 @@ public class LevelControl : MonoBehaviour
     private GameObject[] arTile;
     private int[] pole;
     private int indMaxZn = 1;
-    private int[] arZn = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2096};
+    private int[] arZn = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048};
 
     // Start is called before the first frame update
     void Start()
@@ -37,19 +37,182 @@ public class LevelControl : MonoBehaviour
         {
             n = freeCeils[Random.Range(0, freeCeils.Count)];
             zn = arZn[Random.Range(0, indMaxZn)];
-            Vector3 pos = new Vector3(-3 + 2 * (n % 4), 0, -3 + 2 * (n / 4));
+            Vector3 pos = new Vector3(-3 + 2 * (n % 4), 3, -3 + 2 * (n / 4));
             Color col = Color.cyan;
             GameObject tile = Instantiate(tilePrefab, pos, Quaternion.identity);
             tile.GetComponent<TileControl>().SetNumber(zn, col);
+            pole[n] = zn;
+            arTile[n] = tile;
+            pos.y = 0.5f;
+            tile.GetComponent<TileControl>().SetTarget(pos);
         }
         else
         {   //  Game over
 
         }
     }
+    private void UpdateTile(int num_pos, int tileZn)
+    {
+        //int i, n, zn = 1;
+        int zn = tileZn * 2;
+        if (zn > arZn[indMaxZn]) indMaxZn++;
+        if (indMaxZn >= arZn.Length)
+        {   //  Win
+
+        }
+        else
+        {
+            Vector3 pos = new Vector3(-3 + 2 * (num_pos % 4), 0.5f, -3 + 2 * (num_pos / 4));
+            Color col = Color.cyan;
+            GameObject tile = Instantiate(tilePrefab, pos, Quaternion.identity);
+            tile.GetComponent<TileControl>().SetNumber(zn, col);
+            pole[num_pos] = zn;
+            arTile[num_pos] = tile;
+        }
+    }
 
     public void OnClickDirection(int n)
     {
+        int i;
+        if (n == 2)
+        {
+            for (i = 4; i < 16; i++)
+            {
+                if (pole[i] > 0)
+                {
+                    TileControl tc = arTile[i].GetComponent<TileControl>();
+                    if (i / 4 == 1)
+                    {
+                        if (pole[i - 4] == 0) MoveTile(i, i - 4); //tc.SetTarget(new Vector3(-3 + 2 * (i % 4), 0, -3));
+                        else
+                        {
+                            if (pole[i - 4] == pole[i])
+                            {
+                                pole[i] = 0; Destroy(arTile[i]); Destroy(arTile[i - 4]);
+                                UpdateTile(i - 4, pole[i - 4]);
+                            }
+                        }
+                    }
+                    if (i / 4 == 2)
+                    {
+                        if (pole[i - 4] == 0)
+                        {
+                            if (pole[i - 8] == 0) MoveTile(i, i - 8); //tc.SetTarget(new Vector3(-3 + 2 * (i % 4), 0, -3));
+                            else
+                            {
+                                if (pole[i - 8] == pole[i])
+                                {
+                                    pole[i] = 0; Destroy(arTile[i]); Destroy(arTile[i - 8]);
+                                    UpdateTile(i - 8, pole[i - 8]);
+                                }
+                                else
+                                {
+                                    MoveTile(i, i - 4);
+                                    //tc.SetTarget(new Vector3(-3 + 2 * (i % 4), 0, -1));
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if (pole[i - 4] == pole[i])
+                            {
+                                pole[i] = 0; Destroy(arTile[i]); Destroy(arTile[i - 4]);
+                                UpdateTile(i - 4, pole[i - 4]);
+                            }
+                        }
+                    }
+                    if (i / 4 == 3)
+                    {
+                        if (pole[i - 4] == 0)
+                        {
+                            if (pole[i - 8] == 0)
+                            {
+                                if (pole[i - 12] == 0) MoveTile(i, i - 12); //tc.SetTarget(new Vector3(-3 + 2 * (i % 4), 0, -3));
+                                else
+                                {
+                                    if (pole[i - 12] == pole[i])
+                                    {
+                                        pole[i] = 0; Destroy(arTile[i]); Destroy(arTile[i - 12]);
+                                        UpdateTile(i - 12, pole[i - 12]);
+                                    }
+                                    else
+                                    {
+                                        MoveTile(i, i - 8);
+                                        //tc.SetTarget(new Vector3(-3 + 2 * (i % 4), 0, 1));
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                if (pole[i - 8] == pole[i])
+                                {
+                                    pole[i] = 0; Destroy(arTile[i]); Destroy(arTile[i - 8]);
+                                    UpdateTile(i - 8, pole[i - 8]);
+                                }
+                                else
+                                {
+                                    MoveTile(i, i - 4);
+                                    //tc.SetTarget(new Vector3(-3 + 2 * (i % 4), 0, -1));
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if (pole[i - 4] == pole[i])
+                            {
+                                pole[i] = 0; Destroy(arTile[i]); Destroy(arTile[i - 4]);
+                                UpdateTile(i - 4, pole[i - 4]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        Povtor(n);
+        GenerateTile();
+    }
 
+    private void Povtor(int direction)
+    {
+        for(int i = 0; i < 16; i++)
+        {
+            if (pole[i] > 0)
+            {
+                if (((i % 4) < 3) && (pole[i] == pole[i + 1]))
+                {
+                    if (direction == 3)
+                    {
+                        pole[i] = 0; Destroy(arTile[i]); Destroy(arTile[i + 1]);
+                        UpdateTile(i + 1, pole[i + 1]);
+                    }
+                    else
+                    {
+                        pole[i + 1] = 0; Destroy(arTile[i]); Destroy(arTile[i + 1]);
+                        UpdateTile(i, pole[i]);
+                    }
+                }
+                if (((i / 4) < 3) && (pole[i] == pole[i + 4]))
+                {
+                    if (direction == 0)
+                    {
+                        pole[i] = 0; Destroy(arTile[i]); Destroy(arTile[i + 4]);
+                        UpdateTile(i + 4, pole[i + 4]);
+                    }
+                    else
+                    {
+                        pole[i + 4] = 0; Destroy(arTile[i]); Destroy(arTile[i + 4]);
+                        UpdateTile(i, pole[i]);
+                    }
+                }
+            }
+        }
+    }
+
+    private void MoveTile(int src, int dst)
+    {
+        TileControl tc = arTile[src].GetComponent<TileControl>();
+        tc.SetTarget(new Vector3(-3 + 2 * (dst % 4), 0.5f, -3 + 2 * (dst / 4)));
+        pole[dst] = pole[src];pole[src] = 0;
+        arTile[dst] = arTile[src];arTile[src] = null;
     }
 }
--- Quadrat2048/Assets/Scripts/TileControl.cs
+++ Quadrat2048/Assets/Scripts/TileControl.cs
@@ -7,24 +7,44 @@ public class TileControl : MonoBehaviour
 {
     [SerializeField] private Text txtNumber;
     [SerializeField] private Image imgTile;
-    [SerializeField] private float speed = 3f;
+    [SerializeField] private float speed = 5f;
 
     private bool isMove = false;
     private Vector3 target;
+    private int number;
+
+    public int Number { get { return number; } }
+
     // Start is called before the first frame update
     void Start()
     {
-        target = transform.position;
+        //target = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isMove)
+        {
+            if (Vector3.Distance(transform.position, target) <= 0.1f)
+            {
+                isMove = false;
+                transform.position = target;
+                /*currentPointIndex++;
+                if (points.Length > 0) currentPointIndex %= points.Length;
+                target = points[currentPointIndex];*/
+            }
+            else
+            {
+                Vector3 delta = target - transform.position;
+                transform.Translate(delta.normalized * speed * Time.deltaTime, Space.World);
+            }
+        }
     }
 
     public void SetNumber(int zn, Color col)
     {
+        number = zn;
         txtNumber.text = zn.ToString();
         txtNumber.color = col;
     }
